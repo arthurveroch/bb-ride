b9548387f06bb74bd74bf6054811534fe84a45a0
arthurveroch <verhofstadtpro@gmail.com>
2026-05-16 15:12:47 +0200

    feat: add professionals landing page with dynamic content fetch from Payload CMS

--- src/app/(frontend)/professionnels/page.tsx
+++ src/app/(frontend)/professionnels/page.tsx
@@ -11,8 +11,6 @@ import Histoire from '../components/professionnals-page/histoire/Histoire'
 import Arguments from '../components/home-page/arguments/Arguments'
 import { unstable_cache } from 'next/cache'
 
-export const revalidate = 604800
-
 const getPage = unstable_cache(
   async () => {
     const payload = await getPayload({ config })
