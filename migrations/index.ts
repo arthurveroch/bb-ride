512ab7516104efb8302829d4001940bbd85ad76a
arthurveroch <verhofstadtpro@gmail.com>
2026-05-27 22:03:06 +0200

    feat: add database migration for users, media, and session management schemas

--- migrations/index.ts
+++ migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20260516_134327 from './20260516_134327';
+import * as migration_20260527_200256 from './20260527_200256';
 
 export const migrations = [
   {
     up: migration_20260516_134327.up,
     down: migration_20260516_134327.down,
-    name: '20260516_134327'
+    name: '20260516_134327',
+  },
+  {
+    up: migration_20260527_200256.up,
+    down: migration_20260527_200256.down,
+    name: '20260527_200256'
   },
 ];
